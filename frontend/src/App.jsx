ac711f4eb0682aee896e9799d216194521d0a92f
ankit013002 <112412889+ankit013002@users.noreply.github.com>
2025-04-11 12:24:14 -0400

    Updated fronted to conduct Socket IO.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,41 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { useEffect, useState } from "react";
 import "./App.css";
+import { io } from "socket.io-client";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [connection, setConnection] = useState(false);
+  const [socket, setSocket] = useState();
+
+  useEffect(() => {
+    const connectionString =
+      "http://" + import.meta.env.VITE_HOST + ":" + import.meta.env.VITE_PORT;
+    const socket = io(connectionString);
+    setSocket(socket);
+
+    socket.on("connect", () => {
+      console.log("Connected to Server");
+      setConnection(true);
+    });
+
+    socket.on("message", (data) => {
+      console.log("Message from server:", data);
+    });
+    return () => {
+      socket.disconnect();
+    };
+  }, []);
+
+  const handleClick = () => {
+    console.log(socket);
+    socket.emit("message", "Hi");
+  };
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <p>{connection.toString()}</p>
+        <button onClick={() => handleClick()}>Button</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
